b18cebde14fd865ad36fd1ef13cf835cb601a15a
arvindsrinivas1 <arvindsrinivas96@gmail.com>
2018-06-28 09:44:34 -0700

    Heroku-deploy

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -5,10 +5,16 @@ class Claim < ApplicationRecord
 				   :"4" => [{"procedure_type": "Post mortem", "cost": "$10"}],
 				   :"5" => [{"procedure_type": "Tranqualizer", "cost": "$7"}]}
 
+	AVAILABLE_CLAIMS = ["X_RAY","MRI-SCAN","Blood test","Post Mortem", "Tranqualizer","Radiation"]
+	
 
 	def self.get_claims_data	
 		CLAIMS_DATA
 	end
+
+	def self.get_available_claims
+		AVAILABLE_CLAIMS	
+	end	
 					   
 end
 
